331b4c1fa977706a03ddc692fa0098925f238837
Luís Felipe Silva Ferreira <152612333+LipeAprendiz@users.noreply.github.com>
2024-05-24 09:38:32 -0300

    mandando a boa

--- aprendendo pygame_ex01.py
+++ aprendendo pygame_ex01.py
@@ -17,11 +17,15 @@ barulho_colisao = pygame.mixer.Sound('smw_coin.wav')
 # Definindo o tamanho da janela do jogo.
 largura = 640
 altura = 480
-x = int(largura/2)
-y = int(altura/2)   
+x_snake = int(largura/2)
+y_snake = int(altura/2)   
 
-x_azul = randint(40, 600)
-y_azul = randint(50, 430)
+velocidade = 10
+x_controle = velocidade
+y_controle = 0
+
+x_maca = randint(40, 600)
+y_maca = randint(50, 430)
 
 pontos = 0
 fonte = pygame.font.SysFont('arial', 40, bold=True, italic=True)
@@ -29,45 +33,120 @@ fonte = pygame.font.SysFont('arial', 40, bold=True, italic=True)
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Snake Game 0.1')
 relogio = pygame.time.Clock()
+lista_snake = []
+comprimento_inicial = 5
+morreu = False
+
+def aumenta_snake(lista_snake):
+    for XeY in lista_snake:
+        #XeY = [x, y]
+        #XeY[0] = x
+        #XeY[1] = y
+
+        pygame.draw.rect(tela, (0,255,0), (XeY[0], XeY[1], 20, 20))
+
+def reiniciar_jogo():
+    global pontos, comprimento_inicial, x_snake, y_snake, lista_snake, lista_cabeca, x_maca, y_maca, morreu
+    pontos = 0
+    comprimento_inicial = 5
+    x_snake = int(largura/2)
+    y_snake = int(altura/2)
+    lista_snake = []
+    lista_cabeca = []
+    x_maca = randint(40, 600)
+    y_maca = randint(50, 430)
+    morreu = False
 
 # Fazendo um loop para o jogo poder se atualizar automaticamente quando o jogo iniciar.
 while True:
     relogio.tick(30)
-    tela.fill((0,0,0))
+    tela.fill((255,255,255))
     mensagem = f'Pontos: {pontos}'
-    testo_formatado = fonte.render(mensagem, True, (255,255,255))
+    testo_formatado = fonte.render(mensagem, True, (0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-        '''
-       if event.type == KEYDOWN:
+
+        if event.type == KEYDOWN:
             if event.key == K_a:
-                x = x - 20
+                if x_controle == velocidade:
+                    pass
+                else:
+                    x_controle = -velocidade
+                    y_controle = 0
             if event.key == K_d:
-                x = x + 20
+                if x_controle == -velocidade:
+                    pass
+                else:
+                    x_controle = velocidade
+                    y_controle = 0
             if event.key == K_w:
-                y = y - 20
+                if y_controle == velocidade:
+                    pass
+                else:
+                    y_controle = -velocidade
+                    x_controle = 0
             if event.key == K_s:
-                y = y + 20'''
+                if y_controle == -velocidade:
+                    pass
+                else:
+                    y_controle = velocidade
+                    x_controle = 0  
 
-    if pygame.key.get_pressed()[K_a]:
-        x = x - 20
-    if pygame.key.get_pressed()[K_d]:
-        x = x + 20
-    if pygame.key.get_pressed()[K_w]:
-        y = y - 20
-    if pygame.key.get_pressed()[K_s]:
-        y = y + 20
+    x_snake = x_snake + x_controle
+    y_snake = y_snake + y_controle
 
-    ret_vermelho = pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
-    ret_azul = pygame.draw.rect(tela, (0,0,255), (x_azul,y_azul,40,50))
+    snake = pygame.draw.rect(tela, (0,255,0), (x_snake,y_snake,20,20))
+    maca = pygame.draw.rect(tela, (255,0,0), (x_maca,y_maca,20,20))
 
-    if ret_vermelho.colliderect(ret_azul):
-        x_azul = randint(40, 600)
-        y_azul = randint(50, 430)
+    if snake.colliderect(maca):
+        x_maca = randint(40, 600)
+        y_maca = randint(50, 430)
         pontos = pontos + 1
         barulho_colisao.play()
+        comprimento_inicial = comprimento_inicial + 1
     
+    lista_cabeca = []
+    lista_cabeca.append(x_snake)
+    lista_cabeca.append(y_snake)
+
+    lista_snake.append(lista_cabeca)
+
+    if lista_snake.count(lista_cabeca) > 1:
+        fonte2 = pygame.font.SysFont('arial', 20, bold=True, italic=True)
+        mensagem = 'Game Over! Pressione a tecla R para jogar novamente'
+        testo_formatado = fonte2.render(mensagem, True, (0,0,0))
+        ret_texto = testo_formatado.get_rect()
+
+        morreu = True
+        while morreu:
+            tela.fill((255,255,255))
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_r:
+                        reiniciar_jogo()
+
+            ret_texto.center = (largura//2, altura//2)
+            tela.blit(testo_formatado, ret_texto)
+            pygame.display.update()
+
+    if x_snake > largura:
+        x_snake = 0
+    if x_snake < 0:
+        x_snake = largura
+    if y_snake < 0:
+        y_snake = altura
+    if y_snake > altura:
+        y_snake = 0
+
+    if len(lista_snake) > comprimento_inicial:
+        del lista_snake[0]
+
+    aumenta_snake(lista_snake)
+
     tela.blit(testo_formatado, (450,40))
     pygame.display.update()
